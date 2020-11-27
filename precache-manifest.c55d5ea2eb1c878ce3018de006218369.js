162932ae1e57b2b012bce82d37ef6a3c2e1f3025
bradlss <75090495+bradlss@users.noreply.github.com>
2020-11-27 02:13:49 +0100

    Add files via upload

---
+++ precache-manifest.c55d5ea2eb1c878ce3018de006218369.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf8ceef40962e78aa56d244564a1772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f6ec8f4f48f8cdafef2",
+    "url": "/static/css/main.70a7ecfd.chunk.css"
+  },
+  {
+    "revision": "2228595c5bc57210eec1",
+    "url": "/static/js/2.ae2de708.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.ae2de708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6ec8f4f48f8cdafef2",
+    "url": "/static/js/main.b8d0c513.chunk.js"
+  },
+  {
+    "revision": "dcc534e5adf1fce6f480",
+    "url": "/static/js/runtime-main.5f677427.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
